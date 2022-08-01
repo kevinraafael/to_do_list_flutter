77a1d4ec07faf749de77e4f7bc2e3a84c078450e
Kevin Francelino <kevin.francelino@grupocard.com.br>
2022-08-01 15:28:57 -0400

    feat:model to to do

--- lib/models/to_do.dart
+++ lib/models/to_do.dart
@@ -3,4 +3,14 @@ class ToDo {
   DateTime dateTime;
 
   ToDo({required this.title, required this.dateTime});
+  ToDo.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        dateTime = DateTime.parse(json['dateTime']);
+
+  toJson() {
+    return {
+      'title': title,
+      'dateTime': dateTime.toIso8601String(),
+    };
+  }
 }
