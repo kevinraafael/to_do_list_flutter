07a054b4f535ce689c40c535d5ccc88a02113e01
Kevin Francelino <kevin.francelino@grupocard.com.br>
2022-08-01 15:27:13 -0400

    feat:cores do app

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/painting.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xff365C7D);
+  static const primaryText = Color(0xff69737D);
+  static const taskTitle = Color(0xff1F3447);
+  static const tasksText = Color(0xff343A40);
+  static const snackBar = Color(0xffF1F3F5);
+  static const delete = Color(0xFFF44336);
+}
